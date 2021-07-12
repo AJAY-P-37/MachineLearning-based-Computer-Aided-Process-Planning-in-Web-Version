3076ba002a23049e1b320f504cb14be68b425089
AJAY-P-37 <ajaykri3.7@gmail.com>
2021-07-13 01:38:16 +0530

    yess

--- CAPP_Working.js
+++ CAPP_Working.js
@@ -6,6 +6,9 @@ const dimensionsList = document.getElementById("dimensionslist");
 let backendProcessList = [];
 let backendDimensionList = [];
 
+inputProcess.addEventListener("click", e => {
+    inputProcess.value = null
+})
 inputProcess.addEventListener("change", (e) => {
     console.log(inputProcessList.getElementsByTagName("li").length, dimensionsList.getElementsByTagName("li").length)
     if (inputProcessList.getElementsByTagName("li").length == dimensionsList.getElementsByTagName("li").length) {
@@ -18,26 +21,17 @@ inputProcess.addEventListener("change", (e) => {
         else if (selected == "null") {
             return
         }
+        else {
+            removeDimension();
+        }
         tempList.innerHTML = selected;
         inputProcessList.appendChild(tempList)
     }
     else {
         alert("Update The Dimensions for Previous Process")
     }
-    inputProcess.value = "-- Please Select --";
-    inputProcess.innerText = "-- Please Select --"
-
 })
-function createNewListOfDimensions(diameter, length, offset, minDiameter = null) {
-    const tempList = document.createElement("li");
-    if (minDiameter == null) {
-        tempList.innerHTML = `Diameter=${diameter}, Length=${length}, Offset=${offset}`;
-    } else {
-        tempList.innerHTML = `Major Diameter${diameter}, Minor Diameter${minDiameter}, Length${length}, Offset${offset}`;
-    }
-    dimensionsList.appendChild(tempList)
 
-}
 
 let diameter = 0, minDiameter = null, length = 0, offset = 0;
 const inputDia = document.getElementById("dimensions1");
@@ -61,6 +55,7 @@ inputOff.addEventListener("keyup", (e) => {
         console.log(offset)
         if (inputProcessList.getElementsByTagName("li").length - 1 == dimensionsList.getElementsByTagName("li").length) {
             createNewListOfDimensions(diameter, length, offset, minDiameter);
+            clearPlaceHolders()
         } else {
             alert("Update the 'Process' for the Dimensions")
         }
@@ -68,8 +63,44 @@ inputOff.addEventListener("keyup", (e) => {
 
 })
 
+let newInput = null;
+
+function createNewListOfDimensions(diameter, length, offset, minDiameter = null) {
+
+
+    inputDia.placeholder = "Dia";
+
+    const tempList = document.createElement("li");
+    if (minDiameter == null) {
+        tempList.innerHTML = `Diameter=${diameter}, Length=${length}, Offset=${offset}`;
+    } else {
+        tempList.innerHTML = `Major Diameter=${diameter}, Minor Diameter=${minDiameter}, Length=${length}, Offset=${offset}`;
+        inputDia.placeholder = "Max Dia";
+    }
+    dimensionsList.appendChild(tempList);
+
+    inputDia.value = "";
+
+    inputLen.value = "";
+    inputLen.placeholder = "Len";
+
+    inputOff.value = "";
+    inputOff.placeholder = "Offset";
+
+    newInput.value = "";
+    newInput.placeholder = "Min Dia"
+
+}
+
+function removeDimension() {
+    if (newInput != null) {
+        inputDia.parentNode.removeChild(newInput);
+        inputDia.placeholder = "Dia";
+    }
+}
+
 function addAnotherDimension() {
-    let newInput = document.createElement("input");
+    newInput = document.createElement("input");
     newInput.type = "number";
     newInput.min = "0";
     newInput.max = "300";
