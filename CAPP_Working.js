468cbc39c4d4b29dc25c79f7fb78eb7090cf0116
AJAY-P-37 <ajaykri3.7@gmail.com>
2021-07-13 19:55:53 +0530

    hmm

--- CAPP_Working.js
+++ CAPP_Working.js
@@ -35,35 +35,99 @@ inputProcess.addEventListener("change", (e) => {
 
 let diameter = 0, minDiameter = null, length = 0, offset = 0;
 const inputDia = document.getElementById("dimensions1");
-inputDia.addEventListener("keyup", (e) => {
-    if (e.key == "Enter") {
-        diameter = inputDia.value
-        console.log(diameter)
-    }
-})
 const inputLen = document.getElementById("dimensions2");
-inputLen.addEventListener("keyup", (e) => {
-    if (e.key == "Enter") {
-        length = inputLen.value
-        console.log(length)
-    }
-})
 const inputOff = document.getElementById("dimensions3");
-inputOff.addEventListener("keyup", (e) => {
-    if (e.key == "Enter") {
-        offset = inputOff.value
-        console.log(offset)
+
+let newInput = null;
+// function validateAllInputs() {
+//     let validated = true;
+//     for (let i of [inputDia, inputLen, inputOff]) {
+//         i.addEventListener("input", e => {
+//             i.setCustomValidity('');
+//             if (i.checkValidity())
+//                 validated = true;
+//         })
+//         i.addEventListener("invalid", (e) => {
+//             i.setCustomValidity("Please Enter value Between 0 to 300");
+//             validated = false;
+//         })
+
+//     }
+//     if (inputDia.checkValidity()) {
+//         validated = true;
+//     }
+//     inputDia.addEventListener("invalid", e => {
+//         inputDia.setCustomValidity("Please Enter value Between 0 to 300")
+//         validated = false;
+//     })
+//     if (newInput != null) {
+//         if (newInput.checkValidity()) {
+//             validated = true;
+//         }
+//         newInput.addEventListener("invalid", e => {
+//             newInput.setCustomValidity("Please Enter value Between 0 to 300")
+//             validated = false;
+//         })
+//     }
+//     return validated;
+// }
+
+function validateAllInputs() {
+    let validated = true;
+    // inputDia.addEventListener("input", e => {
+    //     validated = inputDia.checkValidity();
+    // })
+    // inputDia.addEventListener("invalid", (e) => {
+    //     console.log("shit")
+    // })
+    // inputLen.addEventListener("input", e => {
+    //     validated = inputLen.checkValidity();
+    // })
+    // inputLen.addEventListener("invalid", (e) => {
+    //     console.log("shit")
+    // })
+    // inputOff.addEventListener("input", e => {
+    //     validated = inputOff.checkValidity();
+    // })
+    // inputOff.addEventListener("invalid", (e) => {
+    //     console.log("shit")
+    // })
+    if (inputDia.value == "" && inputDia < 0 && inputDia > 300) {
+        validated = false;
+    }
+    return validated;
+}
+console.log(inputDia.oninput = validateAllInputs());
+const enterButton = document.getElementById("enterbutton");
+enterButton.disabled = inputDia.checkValidity() && inputLen.checkValidity() && inputOff.checkValidity();
+enterButton.addEventListener("click", e => {
+
+    if (validateAllInputs()) {
+        diameter = inputDia.value;
+        length = inputLen.value;
+        offset = inputOff.value;
+        if (newInput != null) {
+            minDiameter = newInput.value;
+        } else {
+            minDiameter = null;
+        }
         if (inputProcessList.getElementsByTagName("li").length - 1 == dimensionsList.getElementsByTagName("li").length) {
             createNewListOfDimensions(diameter, length, offset, minDiameter);
-            clearPlaceHolders()
         } else {
             alert("Update the 'Process' for the Dimensions")
         }
     }
+    enterButton.disabled = true;
+
 
 })
+document.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+        e.preventDefault();
+        return false;
+    }
 
-let newInput = null;
+});
 
 function createNewListOfDimensions(diameter, length, offset, minDiameter = null) {
 
@@ -77,6 +141,7 @@ function createNewListOfDimensions(diameter, length, offset, minDiameter = null)
         tempList.innerHTML = `Major Diameter=${diameter}, Minor Diameter=${minDiameter}, Length=${length}, Offset=${offset}`;
         inputDia.placeholder = "Max Dia";
     }
+    console.log(tempList)
     dimensionsList.appendChild(tempList);
 
     inputDia.value = "";
@@ -87,8 +152,10 @@ function createNewListOfDimensions(diameter, length, offset, minDiameter = null)
     inputOff.value = "";
     inputOff.placeholder = "Offset";
 
-    newInput.value = "";
-    newInput.placeholder = "Min Dia"
+    if (newInput != null) {
+        newInput.value = "";
+        newInput.placeholder = "Min Dia"
+    }
 
 }
 
@@ -97,6 +164,8 @@ function removeDimension() {
         inputDia.parentNode.removeChild(newInput);
         inputDia.placeholder = "Dia";
     }
+    newInput = null;
+    minDiameter = null;
 }
 
 function addAnotherDimension() {
@@ -104,15 +173,16 @@ function addAnotherDimension() {
     newInput.type = "number";
     newInput.min = "0";
     newInput.max = "300";
+    newInput.step = "0.01";
     inputDia.placeholder = "MaxDia";
     newInput.placeholder = "MinDia";
-    console.log(inputDia.parentNode)
+    newInput.classList.add("form-control")
     inputDia.parentNode.insertBefore(newInput, inputLen);
 
-    newInput.addEventListener("keyup", (e) => {
-        if (e.key == "Enter") {
-            minDiameter = newInput.value
-            console.log(minDiameter)
-        }
+    newInput.addEventListener("input", e => {
+        newInput.checkValidity();
+    })
+    newInput.addEventListener("invalid", e => {
+        console.log("shit")
     })
 }
